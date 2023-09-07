695a016046788a9f4c556190470f1228a47e399c
ALSAN <125503777+ALSANhub@users.noreply.github.com>
2023-09-07 15:29:48 -0300

    boh

---
+++ aula8.js
@@ -0,0 +1,12 @@
+const nome =  'JHONI FELIPE';
+const sobreNome = 'ALMEIDA SANTOS';
+const idade = 29;
+const peso = 72;
+const altura = 1.80;
+let IMC = 72 / (1.80 * 1.80);
+let anoNasc = 1993;
+
+
+console.log(nome, sobreNome, 'tem',  idade  , 'anos,  pesa',
+peso , 'kg, e tem',  altura , 'de altura e seu IMC é de',  IMC ,
+'.',  nome,  sobreNome , 'nasceu em',  anoNasc)
